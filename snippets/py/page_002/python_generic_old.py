cd0c214e1dd5a73a84b684498f072961d0957b26
Selvaganapathi-A <selvaganapathi066@gmail.com>
2024-02-23 10:38:34 +0530

    new file for "generic" added.

--- snippets/py/page_002/python_generic_old.py
+++ snippets/py/page_002/python_generic_old.py
@@ -1,9 +1,13 @@
 from decimal import Decimal
+from typing import Callable, TypeAlias
 
 from typing_extensions import TypeVar
 
 # * Generic bound to int, float, str, Decimal
 T = TypeVar("T", int, float, str, Decimal)
+
+A: TypeAlias = tuple[int, int]
+function: TypeAlias = Callable[[int, int, int], int]
 
 
 def lambda_add(a: T, b: T) -> T:
@@ -11,9 +15,13 @@ def lambda_add(a: T, b: T) -> T:
 
 
 def main():
+    xd: A = (9, 8)
+    print(xd)
     pass
 
 
 if __name__ == "__main__":
+    # help(TypeAlias)
     main()
+
     pass
