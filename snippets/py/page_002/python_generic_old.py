08b5c6680f3d7924a8acd8df727194c62a322a54
Selvaganapathi-A <61277482+Selvaganapathi-A@users.noreply.github.com>
2024-02-23 10:48:55 +0530

    add function annotation

--- snippets/py/page_002/python_generic_old.py
+++ snippets/py/page_002/python_generic_old.py
@@ -7,14 +7,24 @@ from typing_extensions import TypeVar
 T = TypeVar("T", int, float, str, Decimal)
 
 A: TypeAlias = tuple[int, int]
-function: TypeAlias = Callable[[int, int, int], int]
+function: TypeAlias = Callable[[int, int, int], float]
 
 
 def lambda_add(a: T, b: T) -> T:
     return a + b
 
 
+def green(x: int, y: int, z: int) -> float:
+    return (x + y + z) / 3
+
+
 def main():
+    yellow: function = green
+
+    print(yellow)
+    print(yellow.__annotations__)
+    print(yellow(7, 9, 8))
+
     xd: A = (9, 8)
     print(xd)
     pass
