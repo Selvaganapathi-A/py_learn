b9ae1fb35a1a4cc7442156e62b5619ed0e2854b9
Selvaganapathi-A <selvaganapathi066@gmail.com>
2024-02-25 19:20:21 +0530

    add context manager

--- snippets/py/page_001/python_context_managers_001.py
+++ snippets/py/page_001/python_context_managers_001.py
@@ -1,10 +1,16 @@
-import logging
-import time
+from dataclasses import dataclass
 from time import perf_counter_ns
+
+#
+#
+#
+#
+#
 from types import TracebackType
 
-from dataclasses import dataclass
-
+import asyncio
+import logging
+import time
 """
 With context managers, you can perform any pair of operations that needs to be done
 before and after another operation or procedure, such as:
@@ -17,6 +23,63 @@ Enter and exit
 Start and stop
 Setup and teardown
 """
+
+
+class CustomContextManager:
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+        print("entering")
+        return self
+
+    def __exit__(self, exc_type: BaseException, exc: type[BaseException],
+                 exc_tb: TracebackType):
+        end = time.perf_counter()
+        print(end - self.start, 'microseconds in sync mode')
+        return isinstance(exc, ZeroDivisionError)
+
+    def wait(self):
+        time.sleep(2)
+
+
+class AsyncCustomContextManager:
+
+    async def __aenter__(self):
+        print("async entering")
+        self.start = time.perf_counter()
+        return self
+
+    async def __aexit__(self, exc_type: BaseException, exc: type[BaseException],
+                        exc_tb: TracebackType):
+        end = time.perf_counter()
+        print(end - self.start, 'microseconds in async mode.')
+        return isinstance(exc, ZeroDivisionError)
+
+    async def asyncwait(self):
+        await asyncio.sleep(2)
+
+
+async def a_main():
+    with CustomContextManager() as _:
+        print(_)
+        for x in range(5):
+            _.wait()
+        print(81 / 0)
+    #
+    async with AsyncCustomContextManager() as _:
+        print(_)
+        await asyncio.gather(*(_.asyncwait() for x in range(5)))
+        print(361 / 0)
+
+
+if __name__ == "__main__":
+    asyncio.run(a_main())
+
+#
+#
+#
+#
+#
 
 
 @dataclass(slots=True)
