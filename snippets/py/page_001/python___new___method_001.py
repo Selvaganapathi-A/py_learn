1aa43507e312c5995929b14f3cf247a9775b260a
Selvaganapathi-A <selvaganapathi066@gmail.com>
2024-02-29 16:36:09 +0530

    Singleton Enhanced

--- snippets/py/page_001/python___new___method_001.py
+++ snippets/py/page_001/python___new___method_001.py
@@ -1,16 +1,21 @@
-from typing import Any, Self, Type
+from typing import Any, Self, Type, Union
 
 
 class Connection:
-    __spawned__instance__ = None
+    __slots__: tuple[str, ...] = ("__initialized__",)
+    __spawned__instance__: Union[Self, None] = None
 
-    def __new__(cls: Type[Self], *args: Any, **kwargs: Any) -> Self:
+    def __new__(cls, *args: Any, **kwargs: Any) -> Self:
         if cls.__spawned__instance__ is None:
             print("Creating new Connection.")
             cls.__spawned__instance__ = super().__new__(cls)
+            cls.__spawned__instance__.__initialized__ = False
         return cls.__spawned__instance__
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        if self.__initialized__:
+            return
+        self.__initialized__: bool = True
         print("Initializing Connection...")
 
 
